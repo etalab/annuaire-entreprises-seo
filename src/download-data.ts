bc7795a6cb32efd7d58ce67197e606c1881cfe2d
Xavier Jp <xajouppe@gmail.com>
2024-01-08 16:51:07 +0100

    fix: trigger download

--- src/download-data.ts
+++ src/download-data.ts
@@ -8,10 +8,10 @@ export const downloadAndSaveData = async () => {
   console.time('⏱ Download and save base SIREN');
 
   //@ts-ignore
-  // const names = await axios.get(SOURCE_URL, { timeout: 120000 });
-  // const data = names.data;
+  const names = await axios.get(SOURCE_URL, { timeout: 120000 });
+  const data = names.data;
   const filePath = `/tmp/sitemap-${new Date().getTime()}`;
-  await fs.promises.writeFile(filePath, 'data', 'utf-8');
+  await fs.promises.writeFile(filePath, data, 'utf-8');
 
   console.timeEnd('⏱ Download and save base SIREN');
 
